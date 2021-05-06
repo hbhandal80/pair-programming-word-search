d11a608c91c6f15b6163dd031e6a5136075ccd4d
Hervinder <hbhandal80@hotmail.com>
2021-05-06 06:45:58 +0000

    Completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    for (const l of horizontalJoin) {
+        if (l.includes(word) === true) {
+            return true;
+        } 
     }
-}
-
-module.exports = wordSearch
+    let arr = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        let newArr = [];
+        for (let j = 0; j < letters.length; j++) {
+          newArr.push(letters[j][i]);
+        }
+        arr.push(newArr);
+      }
+      const verticalJoin = arr.map(item => item.join(''))
+      for (const string of verticalJoin) {
+        if (string.includes(word) === true) {
+            return true;
+        } 
+    }
+    return false;
+    }
+  
+  
+  module.exports = wordSearch;
+  
